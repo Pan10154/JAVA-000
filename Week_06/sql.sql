dc89a92acbe51453af8521554f77ffacd0f7f09f
pan10154 <1015473733@qq.com>
2020-11-25 22:45:38 +0800

    提交sql

---
+++ Week_06/sql.sql
@@ -0,0 +1,181 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.5.49 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `tb_brand` */
+
+CREATE TABLE `tb_brand` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL COMMENT '品牌名称',
+  `first_char` varchar(1) DEFAULT NULL COMMENT '品牌首字母',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_goods` */
+
+CREATE TABLE `tb_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `seller_id` varchar(20) DEFAULT NULL COMMENT '商家ID',
+  `goods_name` varchar(100) DEFAULT NULL COMMENT 'SPU名',
+  `default_item_id` bigint(20) DEFAULT NULL COMMENT '默认SKU',
+  `audit_status` varchar(2) DEFAULT NULL COMMENT '状态',
+  `is_marketable` varchar(1) DEFAULT NULL COMMENT '是否上架',
+  `brand_id` bigint(10) DEFAULT NULL COMMENT '品牌',
+  `caption` varchar(100) DEFAULT NULL COMMENT '副标题',
+  `category1_id` bigint(20) DEFAULT NULL COMMENT '一级类目',
+  `category2_id` bigint(10) DEFAULT NULL COMMENT '二级类目',
+  `category3_id` bigint(10) DEFAULT NULL COMMENT '三级类目',
+  `small_pic` varchar(150) DEFAULT NULL COMMENT '小图',
+  `price` decimal(10,2) DEFAULT NULL COMMENT '商城价',
+  `type_template_id` bigint(20) DEFAULT NULL COMMENT '分类模板ID',
+  `is_enable_spec` varchar(1) DEFAULT NULL COMMENT '是否启用规格',
+  `is_delete` varchar(1) DEFAULT NULL COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=149187842868008 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_goods_desc` */
+
+CREATE TABLE `tb_goods_desc` (
+  `goods_id` bigint(20) NOT NULL COMMENT 'SPU_ID',
+  `introduction` varchar(3000) DEFAULT NULL COMMENT '描述',
+  `specification_items` varchar(3000) DEFAULT NULL COMMENT '规格结果集，所有规格，包含isSelected',
+  `custom_attribute_items` varchar(3000) DEFAULT NULL COMMENT '自定义属性（参数结果）',
+  `item_images` varchar(3000) DEFAULT NULL,
+  `package_list` varchar(3000) DEFAULT NULL COMMENT '包装列表',
+  `sale_service` varchar(3000) DEFAULT NULL COMMENT '售后服务',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_item` */
+
+CREATE TABLE `tb_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品id，同时也是商品编号',
+  `title` varchar(100) NOT NULL COMMENT '商品标题',
+  `sell_point` varchar(500) DEFAULT NULL COMMENT '商品卖点',
+  `price` decimal(20,2) NOT NULL COMMENT '商品价格，单位为：元',
+  `stock_count` int(10) DEFAULT NULL,
+  `num` int(10) NOT NULL COMMENT '库存数量',
+  `barcode` varchar(30) DEFAULT NULL COMMENT '商品条形码',
+  `image` varchar(2000) DEFAULT NULL COMMENT '商品图片',
+  `categoryId` bigint(10) NOT NULL COMMENT '所属类目，叶子类目',
+  `status` varchar(1) NOT NULL COMMENT '商品状态，1-正常，2-下架，3-删除',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `cost_pirce` decimal(10,2) DEFAULT NULL,
+  `market_price` decimal(10,2) DEFAULT NULL,
+  `is_default` varchar(1) DEFAULT NULL,
+  `goods_id` bigint(20) DEFAULT NULL,
+  `category` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cid` (`categoryId`),
+  KEY `status` (`status`),
+  KEY `updated` (`update_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1369463 DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+/*Table structure for table `tb_item_cat` */
+
+CREATE TABLE `tb_item_cat` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类目ID',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父类目ID=0时，代表的是一级的类目',
+  `name` varchar(50) DEFAULT NULL COMMENT '类目名称',
+  `type_id` bigint(11) DEFAULT NULL COMMENT '类型id',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1205 DEFAULT CHARSET=utf8 COMMENT='商品类目';
+
+/*Table structure for table `tb_order` */
+
+CREATE TABLE `tb_order` (
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `payment` decimal(20,2) DEFAULT NULL COMMENT '实付金额。',
+  `payment_type` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '支付类型，1、在线支付，2、货到付款',
+  `post_fee` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '邮费',
+  `status` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭,7、待评价',
+  `create_time` datetime DEFAULT NULL COMMENT '订单创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '订单更新时间',
+  `payment_time` datetime DEFAULT NULL COMMENT '付款时间',
+  `consign_time` datetime DEFAULT NULL COMMENT '发货时间',
+  `end_time` datetime DEFAULT NULL COMMENT '交易完成时间',
+  `close_time` datetime DEFAULT NULL COMMENT '交易关闭时间',
+  `shipping_name` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '物流名称',
+  `shipping_code` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '物流单号',
+  `user_id` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '用户id',
+  `buyer_message` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '买家留言',
+  `buyer_nick` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '买家昵称',
+  `buyer_rate` varchar(2) COLLATE utf8_bin DEFAULT NULL COMMENT '买家是否已经评价',
+  `receiver_area_name` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '收货人地区名称(省，市，县)街道',
+  `receiver_mobile` varchar(12) COLLATE utf8_bin DEFAULT NULL COMMENT '收货人手机',
+  `receiver_zip_code` varchar(15) COLLATE utf8_bin DEFAULT NULL COMMENT '收货人邮编',
+  `receiver` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '收货人',
+  `expire` datetime DEFAULT NULL COMMENT '过期时间',
+  `invoice_type` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '发票类型(普通发票，电子发票，增值税发票)',
+  `source_type` varchar(1) COLLATE utf8_bin DEFAULT NULL COMMENT '订单来源：1:app端，2：pc端，3：M端，4：微信端，5：手机qq端',
+  `seller_id` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '商家ID',
+  PRIMARY KEY (`order_id`),
+  KEY `create_time` (`create_time`),
+  KEY `buyer_nick` (`buyer_nick`),
+  KEY `status` (`status`),
+  KEY `payment_type` (`payment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `tb_order_item` */
+
+CREATE TABLE `tb_order_item` (
+  `id` bigint(20) NOT NULL,
+  `item_id` bigint(20) NOT NULL COMMENT '商品id',
+  `goods_id` bigint(20) DEFAULT NULL COMMENT 'SPU_ID',
+  `order_id` bigint(20) NOT NULL COMMENT '订单id',
+  `title` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '商品标题',
+  `price` decimal(20,2) DEFAULT NULL COMMENT '商品单价',
+  `num` int(10) DEFAULT NULL COMMENT '商品购买数量',
+  `total_fee` decimal(20,2) DEFAULT NULL COMMENT '商品总金额',
+  `pic_path` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '商品图片地址',
+  `seller_id` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `tb_user` */
+
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `phone` varchar(20) DEFAULT NULL COMMENT '注册手机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '注册邮箱',
+  `created` datetime NOT NULL COMMENT '创建时间',
+  `updated` datetime NOT NULL,
+  `nick_name` varchar(50) DEFAULT NULL COMMENT '昵称',
+  `name` varchar(50) DEFAULT NULL COMMENT '真实姓名',
+  `status` varchar(1) DEFAULT NULL COMMENT '使用状态（Y正常 N非正常）',
+  `head_pic` varchar(150) DEFAULT NULL COMMENT '头像地址',
+  `qq` varchar(20) DEFAULT NULL COMMENT 'QQ号码',
+  `account_balance` decimal(10,0) DEFAULT NULL COMMENT '账户余额',
+  `is_mobile_check` varchar(1) DEFAULT '0' COMMENT '手机是否验证 （0否  1是）',
+  `is_email_check` varchar(1) DEFAULT '0' COMMENT '邮箱是否检测（0否  1是）',
+  `sex` varchar(1) DEFAULT '0' COMMENT '性别，1男，2女',
+  `birthday` datetime DEFAULT NULL COMMENT '生日',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
