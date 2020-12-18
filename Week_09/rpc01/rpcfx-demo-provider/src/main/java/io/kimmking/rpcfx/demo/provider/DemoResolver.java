2ce87a232090cdfc5e37709799b875ae2f2da57d
pan10154 <1015473733@qq.com>
2020-12-19 00:04:19 +0800

    修改为泛型和反

---
+++ Week_09/rpc01/rpcfx-demo-provider/src/main/java/io/kimmking/rpcfx/demo/provider/DemoResolver.java
@@ -0,0 +1,24 @@
+package io.kimmking.rpcfx.demo.provider;
+
+import io.kimmking.rpcfx.api.RpcfxResolver;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class DemoResolver<T> implements RpcfxResolver<T>, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public Object resolve(String serviceClass) {
+        return this.applicationContext.getBean(serviceClass);
+    }
+
+    public T resolve(Class<T> clazz) {
+        return this.applicationContext.getBean(clazz);
+    }
+}
